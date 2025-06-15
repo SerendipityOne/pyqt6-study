c93e826a8ab3cfa2d6438dacc1ed79600259301d
Serendipity <1402485721@qq.com>
2025-06-15 16:50:44 +0800

    更改文本并禁用按钮，使其不再可点击

--- app2.py
+++ app2.py
@@ -8,35 +8,19 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.button_is_checked = True
-
-        self.setFixedSize(QSize(400, 300))
         self.setWindowTitle("My App")
-
-        # button = QPushButton("Click me")
-        # button.setCheckable(True)
-        # button.clicked.connect(self.the_button_was_clicked)
-        # button.clicked.connect(self.the_button_was_toggled)
+        self.setFixedSize(QSize(400, 200))
 
         self.button = QPushButton("Click me")
-        self.button.setCheckable(True)
-        self.button.released.connect(self.the_button_was_released)
-        self.button.setChecked(self.button_is_checked)
+        self.button.clicked.connect(self.the_button_was_clicked)
 
         self.setCentralWidget(self.button)
 
-    def the_button_was_released(self):
-        self.button_is_checked = self.button.isChecked()
-        print(self.button_is_checked)
-
     def the_button_was_clicked(self):
-        print("Clicked")
+        self.button.setText("You already clicked me!")
+        self.button.setEnabled(False)
 
-    # QPushButton 的 toggled 信号会在按钮的状态改变时被触发，并且会自动传递一个布尔值 checked 给连接的槽（slot）或函数
-    def the_button_was_toggled(self, checked):
-        # print("Checked?", checked)
-        self.button_is_checked = checked
-        print(self.button_is_checked)
+        self.setWindowTitle("My Oneshot App")
 
 
 app = QApplication(sys.argv)
