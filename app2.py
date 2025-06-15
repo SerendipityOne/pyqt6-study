2dbaffec2e8eb910d88f5dc671cf8291023c7e06
Serendipity <1402485721@qq.com>
2025-06-15 17:00:09 +0800

    点击按钮，更改窗口标题，当标题为Something went wrong时，禁止按钮点击

--- app2.py
+++ app2.py
@@ -1,7 +1,20 @@
 import sys
+from random import choice
 
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow
+
+window_titles = [
+    'My App',
+    'My App',
+    'Still My App',
+    'Still My App',
+    'What on earth',
+    'What on earth',
+    'This is surprising',
+    'This is surprising',
+    'Something went wrong'
+]
 
 
 class MainWindow(QMainWindow):
@@ -12,15 +25,25 @@ class MainWindow(QMainWindow):
         self.setFixedSize(QSize(400, 200))
 
         self.button = QPushButton("Click me")
-        self.button.clicked.connect(self.the_button_was_clicked)
+        self.button.clicked.connect(self.the_button_was_clicked)  # 连接按钮点击信号
+        self.windowTitleChanged.connect(self.the_window_title_changed)  # 连接窗口标题改变信号
 
         self.setCentralWidget(self.button)
 
+    # 按钮被点击时调用的槽函数
     def the_button_was_clicked(self):
-        self.button.setText("You already clicked me!")
-        self.button.setEnabled(False)
+        print("Clicked.")
+        new_window_title = choice(window_titles)
+        print("Setting title:  %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
 
-        self.setWindowTitle("My Oneshot App")
+    # 当窗口标题改变时调用的槽函数
+    def the_window_title_changed(self, window_title):
+        print("Window title changed: %s" % window_title)
+
+        if window_title == "Something went wrong":
+            self.button.setText("You can't click me anymore")
+            self.button.setDisabled(True)
 
 
 app = QApplication(sys.argv)
