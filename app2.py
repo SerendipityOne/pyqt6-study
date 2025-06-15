690de222af3fd93b26ffd1930debc4965092909d
Serendipity <1402485721@qq.com>
2025-06-15 17:15:59 +0800

    直接将 widget 连接在一起

--- app2.py
+++ app2.py
@@ -1,16 +1,7 @@
 import sys
-from random import choice
 
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow
-
-window_titles = [
-    'My App',
-    'Still My App',
-    'What on earth',
-    'This is surprising',
-    'Something went wrong'
-]
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
@@ -20,26 +11,19 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
         self.setFixedSize(QSize(400, 200))
 
-        self.button = QPushButton("Click me")
-        self.button.clicked.connect(self.the_button_was_clicked)  # 连接按钮点击信号
-        self.windowTitleChanged.connect(self.the_window_title_changed)  # 连接窗口标题改变信号
+        self.label = QLabel()
 
-        self.setCentralWidget(self.button)
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)  # 将输入框内容绑定到标签上
 
-    # 按钮被点击时调用的槽函数
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print("Setting title:  %s" % new_window_title)
-        self.setWindowTitle(new_window_title)
+        layout = QVBoxLayout()  # 垂直布局
+        layout.addWidget(self.input)  # 添加输入框到布局
+        layout.addWidget(self.label)
 
-    # 当窗口标题改变时调用的槽函数
-    def the_window_title_changed(self, window_title):
-        print("Window title changed: %s" % window_title)
+        container = QWidget()  # 创建容器
+        container.setLayout(layout)  # 设置布局
 
-        if window_title == "Something went wrong":
-            self.button.setText("You can't click me anymore")
-            self.button.setDisabled(True)
+        self.setCentralWidget(container)
 
 
 app = QApplication(sys.argv)
