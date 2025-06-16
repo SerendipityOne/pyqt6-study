0795ac002c5585fa0be305c45cf2bc01301605bc
Serendipity <1402485721@qq.com>
2025-06-16 15:15:47 +0800

    QDial旋钮控件

--- app3.py
+++ app3.py
@@ -20,21 +20,17 @@ from PyQt6.QtWidgets import (
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.setWindowTitle("My App")
 
-        widget = QSlider(Qt.Orientation.Vertical) # 创建垂直方向的滑块（默认）
-        # widget = QSlider(Qt.Orientation.Horizontal) # 创建水平方向的滑块
+        widget = QDial()  # 创建一个旋钮控件
+        widget.setRange(-10, 100)
+        widget.setSingleStep(1)
 
-        widget.setMinimum(-10)
-        widget.setMaximum(3)
-        # Or: widget.setRange(-10,3)
-
-        widget.setSingleStep(3)
-
-        widget.valueChanged.connect(self.value_changed)  # 当滑块的值改变时触发
-        widget.sliderMoved.connect(self.slider_position)  # 当滑块移动时触发
-        widget.sliderPressed.connect(self.slider_pressed)  # 当滑块按下时触发
-        widget.sliderReleased.connect(self.slider_released)  # 当滑块释放时触发
+        widget.valueChanged.connect(self.value_changed)  # 绑定旋钮值改变信号
+        widget.sliderMoved.connect(self.slider_position)  # 绑定滑块位置改变信号
+        widget.sliderPressed.connect(self.slider_pressed)  # 绑定滑块按下信号
+        widget.sliderReleased.connect(self.slider_released)  # 绑定滑块释放信号
 
         self.setCentralWidget(widget)
 
@@ -42,7 +38,7 @@ class MainWindow(QMainWindow):
         print(i)
 
     def slider_position(self, p):
-        print("Position", p)
+        print("position", p)
 
     def slider_pressed(self):
         print("Pressed!")
