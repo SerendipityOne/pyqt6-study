47b22ce03685b43b1e08fb0bb1ff1480047565a4
Serendipity <1402485721@qq.com>
2025-06-16 08:53:15 +0800

    快速演示最常见的 PyQt 小部件

--- app3.py
+++ app3.py
@@ -33,22 +33,22 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout()
         widgets = [
-            QCheckBox,
-            QComboBox,
-            QDateEdit,
-            QDateTimeEdit,
-            QDial,
-            QDoubleSpinBox,
-            QFontComboBox,
-            QLCDNumber,
-            QLabel,
-            QLineEdit,
-            QProgressBar,
-            QPushButton,
-            QRadioButton,
-            QSlider,
-            QSpinBox,
-            QTimeEdit,
+            QCheckBox,  # 复选框
+            QComboBox,  # 下拉列表框
+            QDateEdit,  # 用于编辑日期和日期时间
+            QDateTimeEdit,  # 用于编辑日期和日期时间
+            QDial,  # 可旋转表盘
+            QDoubleSpinBox,  # 浮点数微调器
+            QFontComboBox,  # 字体列表
+            QLCDNumber,  # 相当丑陋的 LCD 显示屏
+            QLabel,  # 只是一个标签，而不是交互式的
+            QLineEdit,  # 输入一行文本
+            QProgressBar,  # 进度条
+            QPushButton,  # 一个按钮
+            QRadioButton,  # 只有一个活动项的切换集
+            QSlider,  # 滑块
+            QSpinBox,  # 整数微调器
+            QTimeEdit,  # 对于编辑时间
         ]
 
         for w in widgets:
