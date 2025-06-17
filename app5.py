aff55e92ccbefa84069e37ab435d7857a246a059
Serendipity <1402485721@qq.com>
2025-06-17 15:31:05 +0800

    添加工具栏

--- app5.py
+++ app5.py
@@ -16,17 +16,32 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
 
         label = QLabel("Hello!")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)  # 设置文本居中
 
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(16, 16))  # 设置图标大小
         self.addToolBar(toolbar)
 
-        button_action = QAction("Your button", self)
+        button_action = QAction(QIcon("bug.png"), "&Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.toolbar_button_clicked)
+        button_action.setCheckable(True)
         toolbar.addAction(button_action)
+
+        toolbar.addSeparator()  # 添加分割线
+
+        button_action2 = QAction(QIcon("bug.png"), "&Your button2", self)
+        button_action2.setStatusTip("This is your button2")
+        button_action2.triggered.connect(self.toolbar_button_clicked)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addWidget(QLabel("Hello!"))
+        toolbar.addWidget(QCheckBox("Check me!"))
+
+        self.setStatusBar(QStatusBar(self))  # 添加状态栏
 
     def toolbar_button_clicked(self, s):
         print("click", s)
