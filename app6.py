1107fe82d4ca4ef9c088d55717ebedefcb92ed95
Serendipity <1402485721@qq.com>
2025-06-17 16:21:46 +0800

    使用QMessageBox的简易提示框

--- app6.py
+++ app6.py
@@ -17,10 +17,12 @@ class MainWindow(QMainWindow):
         dlg = QMessageBox(self)
         dlg.setWindowTitle("I have a question!")
         dlg.setText("This is a simple dialog")
+        dlg.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+        dlg.setIcon(QMessageBox.Icon.Question)
         button = dlg.exec()
 
-        if button == QMessageBox.StandardButton.Ok:
-            print("OK!")
+        if button == QMessageBox.StandardButton.Yes:
+            print("Yes!")
 
 
 app = QApplication(sys.argv)
